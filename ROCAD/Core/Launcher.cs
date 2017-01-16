b9bf5ab71f7b3419a2e614378b025f6acdf41630
Loïs <Los@SIBASAW>
2017-01-16 11:30:30 +0100

    Ajoutez des fichiers projet.

---
+++ ROCAD/Core/Launcher.cs
@@ -0,0 +1,18 @@
+﻿using ROCAD.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROCAD.Core
+{
+    class Launcher
+    {
+        static int Main(string[] args)
+        {
+            
+            return 0;
+        }
+    }
+}
