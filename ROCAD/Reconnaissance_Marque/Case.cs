fe76059e1788e2678e6970f7de8c203f96fa58cc
jy503769 <jy503769@S665P05>
2017-01-18 10:38:22 +0100

    Analyse de la copie etudiant

---
+++ ROCAD/Reconnaissance_Marque/Case.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PT_Lot4
+{
+    public class Case
+    {
+     
+        public static int HEIGHT = 10;
+        public static int WIDTH = 10;
+
+        public Case() {
+         
+        }
+        
+      
+
+
+    }
+}
